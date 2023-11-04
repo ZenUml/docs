79b0dfe8402c8957c43408190bc605bfff3dcb20
Yanhui Li <yanhui.l.li@oracle.com>
2023-11-04 15:27:22 +1100

    Initial workable proxy

---
+++ functions/vendor.js
@@ -0,0 +1,7 @@
+export async function onRequest(event) {
+  const url = new URL(event.request.url);
+  console.log('request url', url.pathname)
+
+  const appURL = new URL(`http://sequence-diagram.zenuml.com/${url.pathname}`)
+  return fetch(appURL, event.request);
+}
