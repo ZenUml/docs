2b0cfba099d187e6acf6e9bdeadcecd99b89e0b1
Yanhui Li <yanhui.l.li@oracle.com>
2023-11-10 17:41:43 +1100

    Reverse proxy supports binary file type

--- functions/[[catchall]].js
+++ functions/[[catchall]].js
@@ -1,3 +1,5 @@
+const TEXT_CONTENT_TYPES = ['text', 'application/json', 'application/xml'];
+
 export async function onRequest(event) {
   const targetDomain =
     event.env.LARASITE_DOMAIN || 'sequence-diagram.zenuml.com';
@@ -20,22 +22,31 @@ export async function onRequest(event) {
   console.log('new url:', newUrl);
 
   const response = await fetch(new URL(newUrl), event.request);
-  const body = await response.text();
 
   const replaceUrl = (s) =>
     s
       .replaceAll(newHttpsOrigin, url.origin)
       .replaceAll(newHttpOrigin, url.origin);
-  const newBody = replaceUrl(body);
+
+  const isTextResponse = () => {
+    const contentType = response.headers.get('content-type');
+    return (
+      contentType &&
+      TEXT_CONTENT_TYPES.find((p) => contentType.toLowerCase().startsWith(p))
+    );
+  };
+
+  const newResponseData = isTextResponse()
+    ? replaceUrl(await response.text())
+    : await response.arrayBuffer();
 
   const newHeaders = new Headers(response.headers);
   for (const [header, value] of newHeaders) {
     newHeaders.set(header, replaceUrl(value));
   }
-
   newHeaders.set('X-Forwarded-By', 'Cloudflare page function');
 
-  return new Response(newBody, {
+  return new Response(newResponseData, {
     status: response.status,
     statusText: response.statusText,
     headers: newHeaders,
